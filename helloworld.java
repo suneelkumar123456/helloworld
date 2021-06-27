66ec843baecf31ddf9980632739e6fbf1e8dc156
suneelkumar123456 <85428924+suneelkumar123456@users.noreply.github.com>
2021-06-27 09:19:24 +0530

    Update helloworld.java

--- helloworld.java
+++ helloworld.java
@@ -1,6 +1,6 @@
 class Simple{  
     public static void main(String args[]){  
      System.out.println("Hello batch21 team");
-      System.out.println("Hello batch16 and batch17 students and kjdjdijfidjdfifijfsntraining practicing 2nd ");
+      System.out.println("Hello batch16 and batch17 students and kjdjdijfidjdfifijfsntraining practicing 3rd ");
     }  
 }  
