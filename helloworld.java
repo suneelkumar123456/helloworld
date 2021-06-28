b693cc9edb2906f144e3da696042346dfe74c1d3
suneelkumar123456 <85428924+suneelkumar123456@users.noreply.github.com>
2021-06-28 11:08:41 +0530

    Update helloworld.java

--- helloworld.java
+++ helloworld.java
@@ -1,6 +1,6 @@
 class Simple{  
     public static void main(String args[]){  
      System.out.println("Hello batch21 team");
-      System.out.println("i am ambika ");
+      System.out.println("i am ambika,practing the jenkins");
     }  
 }  
